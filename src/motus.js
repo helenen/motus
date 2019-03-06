e5b93f86084af608f99173dba748f5f17f1cc42c
ABlesse <adrien.blesse@zenika.com>
2019-03-06 15:33:16 +0100

    levels

--- src/motus.js
+++ src/motus.js
@@ -5,7 +5,7 @@ import {
 } from './game.mjs';
 import { writeGrid, getSubmittedWord } from './window.mjs';
 
-const words = ['azertyu', 'flandre', 'tututut'];
+const words = ['azertyu', 'flandre', 'tututut', 'azertyuiop', 'christophe'];
 const levels = {
   veryEasy: {
     length: 7,
@@ -33,35 +33,38 @@ let selectedLevel = {};
 let history = [];
 let score = 0;
 
-let result = selectWord(words, length);
-let censoredResult = censorWord(result, given);
-
-writeGrid(document, length, history);
-
-document.getElementById('word').innerHTML = censoredResult;
-document.getElementById('score').innerHTML = 'Score : ' + score;
+let result;
+let censoredResult;
 
 document.addEventListener('click', (e) => {
   if (e.target.id === 'submit') {
     let state = {};
-    state.word = getSubmittedWord(document, history.length, length);
+    state.word = getSubmittedWord(document, history.length, selectedLevel.length);
     state.rightSlots = compare(state.word, result);
     state.rightChars = deepCompare(state.word, result);
     history.push(state);
-    writeGrid(document, length, history);
+    writeGrid(document, selectedLevel.length, history);
     if (hasWon(state.word, result)) {
       score += 1;
       document.getElementById('score').innerHTML = 'Score : ' + score;
-      result = selectWord(words, length);
-      censoredResult = censorWord(result, given);
+      result = selectWord(words, selectedLevel.length);
+      censoredResult = censorWord(result, selectedLevel.given);
       document.getElementById('word').innerHTML = censoredResult;
       history = [];
-      writeGrid(document, length, history);
+      writeGrid(document, selectedLevel.length, history);
     } else if (isLastTurn(history.length)) {
       document.getElementById('outcome').innerHTML = 'You lose';
     }
   } else if (e.target.id === 'level') {
     selectedLevel = levels[e.target.name];
+    result = selectWord(words, selectedLevel.length);
+    censoredResult = censorWord(result, selectedLevel.given);
+
+    writeGrid(document, selectedLevel.length, history);
+
+    document.getElementById('word').innerHTML = censoredResult;
+    document.getElementById('score').innerHTML = 'Score : ' + score;
     document.getElementById('levels').setAttribute('hidden', true);
+    document.getElementById('reloadButton').removeAttribute('hidden');
   }
 });
